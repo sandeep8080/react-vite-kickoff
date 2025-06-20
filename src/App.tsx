49063f154631fbf146ed1a22aed35db287915590
Sandeep Garg <sandeepgarg14319@gmail.com>
2025-06-20 13:50:40 +0530

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1>Welcome to the React App Template</h1>
+      <p>This is a simple React TS + Vite + Tailwind setup template.</p>
+      <p>Feel free to modify it as you like!</p>
+    </div>
+  );
+}
+
+export default App;
